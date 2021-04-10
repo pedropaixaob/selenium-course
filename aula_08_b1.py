47e0c59c1809c5ecf8a1668ce1c149a3d1592cd5
pedropaixaob <pedropaixaob@gmail.com>
2021-04-10 14:50:05 -0300

    mouse events

--- aula_08_b1.py
+++ aula_08_b1.py
@@ -30,5 +30,38 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-url = https://selenium.dunossauro.live/caixinha
-b.get(url)
+url = 'https://selenium.dunossauro.live/caixinha'
+browser = Firefox()
+browser.get(url)
+browser.set_window_size(1404, 936)
+
+caixa = browser.find_element_by_id('caixa')
+span = browser.find_element_by_tag_name('span') # quando quisermos desfocar
+
+ac = ActionChains(browser)
+
+def caixinha_colorida(key1, key2=None): # colocamos a key2 para dar a oportunidade de apertar dois botões ao mesmo tempo
+
+    ac.pause(1)
+    ac.key_down(key1)
+    if key2:
+        ac.key_down(key2)
+    ac.move_to_element(caixa)
+    ac.pause(1)
+    ac.click()
+    ac.pause(1)
+    ac.double_click()
+    ac.pause(1)
+    ac.move_to_element(span)
+    if key2:
+        ac.key_up(key2)
+    ac.key_up(key1)
+
+caixinha_colorida(Keys.SHIFT)
+caixinha_colorida(Keys.CONTROL)
+caixinha_colorida(Keys.CONTROL, Keys.SHIFT)
+
+ac.move_to_element(caixa)
+ac.context_click()
+ac.pause(1)
+ac.perform()
